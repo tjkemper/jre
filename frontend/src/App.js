19fbb403fa6bebc9349399005ca1001335b713a1
Taylor Kemper <tjkemper6@gmail.com>
2019-09-16 23:35:47 -0700

    Add navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import JreNav from './components/JreNav';
 
 function App() {
   return (
+    
     <div className="App">
+      <JreNav />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
---
+++ frontend/src/components/JreNav.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import Navbar from 'react-bootstrap/Navbar'
+import Nav from 'react-bootstrap/Nav'
+import jreLogo from '../imgs/jre.png'
+
+class JreNav extends React.Component {
+
+  render() {
+    return (
+      <Navbar bg="light" >
+        <Navbar.Brand href="#home">
+          <img
+            alt=""
+            src={jreLogo}
+            width="50"
+            height="50"
+            className="d-inline-block align-top"
+          />
+          <span class="h1">JRE</span>
+        </Navbar.Brand>
+        <Nav className="mr-auto">
+          <Nav.Link href="#home">Search</Nav.Link>
+          <Nav.Link href="#analytics">Analytics</Nav.Link>
+          <Nav.Link href="#about">About</Nav.Link>
+        </Nav>
+      </Navbar>
+    );
+  }
+}
+
+export default JreNav;
