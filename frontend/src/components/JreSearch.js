2fc55d9876f51beab6b1e33d08e116b76cdfbbe7
Taylor Kemper <tjkemper6@gmail.com>
2019-09-17 17:14:03 -0700

    Display table size. Add columns

--- frontend/src/components/JreSearch.js
+++ frontend/src/components/JreSearch.js
@@ -1,34 +1,57 @@
 import React from 'react';
 import BootstrapTable from 'react-bootstrap-table-next';
 import ToolkitProvider, { Search } from 'react-bootstrap-table2-toolkit';
-import jreData from '../data/Uploads from PowerfulJRE_2019-09-13.json'
+import jreData from '../data/Uploads from PowerfulJRE_2019-09-13.json';
 
 // TODO: Show count (number of videos returned in search)
 // TODO: Sort by date
 // TODO: Sort by view count
+
+// https://react-bootstrap-table.github.io/react-bootstrap-table2/docs/table-props.html
 class JreSearch extends React.Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      dataSize: jreData.videos.length,
+    };
+  }
+
+  handleDataSizeChange({ dataSize }) {
+    this.setState({
+      dataSize: dataSize,
+    });
+  }
+
   render() {
     const { SearchBar } = Search;
     const columns = [
       {
+        dataField: 'url',
+        text: 'Url',
+        formatter: (cell) => <a href={cell} target="_blank">link</a>,
+      }, {
         dataField: 'name',
         text: 'Name',
         sort: true,
       }, {
         dataField: 'datePublished',
         text: 'Date Published',
-      }, {
-        dataField: 'genre',
-        text: 'Genre',
-      }, {
-        dataField: 'keywords',
-        text: 'Keywords',
+        sort: true,
       }, {
         dataField: 'watchViewCount',
         text: 'View Count',
+        sort: true,
       }, {
         dataField: 'duration',
         text: 'Duration',
+        sort: true,
+      }, {
+        dataField: 'description',
+        text: 'Description',
+      },{
+        dataField: 'keywords',
+        text: 'Keywords',
       },
     ];
     
@@ -43,9 +66,11 @@ class JreSearch extends React.Component {
           props => (
             <div>
               <SearchBar { ...props.searchProps } />
+              <div>Results: {this.state.dataSize}</div>
               <hr />
-              <BootstrapTable
+              <BootstrapTable 
                 { ...props.baseProps }
+                onDataSizeChange={ this.handleDataSizeChange.bind(this) }
               />
             </div>
           )
